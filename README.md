2399c1ccd368643c48440934c78dd1b73adca6dd
PGgit08 <peter.mty8@gmail.com>
2024-07-06 22:29:56 -0400

    started messing around with loggers

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 A base project for future robots that has code for swerve drive, PhotonVision processing, and led strip control.
 
 ## Logging
-For logging, the swerve base uses Monologue to log robot-specficic data. Individual devices (motors/encoders) are logged with SignalLogger.
+For logging, the swerve base uses Monologue to log robot-specficic data. SignalLogger and URCL are used to log device-specific (motors/encoders) data.
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,17 @@
 
 package frc.robot;
 
+import org.littletonrobotics.urcl.URCL;
+
+import com.ctre.phoenix6.SignalLogger;
+
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import monologue.Logged;
+import monologue.Monologue;
+import monologue.Annotations.Log;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -14,7 +22,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends TimedRobot implements Logged {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
@@ -28,6 +36,15 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+  
+    Monologue.setupMonologue(this, "Robot", false, false);
+
+    DataLogManager.start();
+    URCL.start();
+    
+    // SignalLogger.setPath("/logs/ctre-logs/"); // not working in sim
+    SignalLogger.start();
   }
 
   /**
@@ -44,6 +61,13 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+
+    Monologue.updateAll();
+  }
+
+  @Log.NT
+  public String bruh() {
+    return "bruh";
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
