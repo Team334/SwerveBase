e8c4c3edd7223e411b2d5842638d61958c59a92f
PGgit08 <peter.mty8@gmail.com>
2024-07-15 15:30:32 -0400

    added more faults to check for a talonfx

--- src/main/java/frc/lib/FaultLogger.java
+++ src/main/java/frc/lib/FaultLogger.java
@@ -3,7 +3,10 @@ package frc.lib;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import frc.lib.selfchecked.SelfChecked;
+import frc.lib.selfchecked.SelfCheckedTalonFX;
 
 /**
  * Devices are added to this fault logger to have their faults alerted and logged.
@@ -11,11 +14,17 @@ import frc.lib.selfchecked.SelfChecked;
 public class FaultLogger {
   private static List<SelfChecked> _devices = new ArrayList<SelfChecked>();
   
-  // TODO: Create all "add" functions here
+  /** Register a new SelfChecked device to the FaultLogger. */
+  public static void register(SelfChecked device) {
+    _devices.add(device);
+  }
 
-  /**
-   * Checks all added devices and alerts their faults.
-   */
+  /** Register a new TalonFX to the FaultLogger. */
+  public static void register(String name, TalonFX talonFX) {
+    register(new SelfCheckedTalonFX(name, talonFX));
+  }
+
+  /** Checks all added devices and alerts their faults. */
   public static void checkAllFaults() {
     for (SelfChecked device : _devices) {
       device.checkFaults();
--- src/main/java/frc/lib/selfchecked/SelfCheckedTalonFX.java
+++ src/main/java/frc/lib/selfchecked/SelfCheckedTalonFX.java
@@ -14,20 +14,28 @@ public class SelfCheckedTalonFX extends SelfChecked {
   private Alert _hardwareFaultAlert;
   private Alert _forwardSoftLimitFaultAlert;
   private Alert _reverseSoftLimitFaultAlert;
+  private Alert _statorCurrentLimitFaultAlert;
+  private Alert _supplyCurrentLimitFaultAlert;
 
   // faults
   private StatusSignal<Boolean> _hardwareFault;
   private StatusSignal<Boolean> _forwardSoftLimitFault;
   private StatusSignal<Boolean> _reverseSoftLimitFault;
+  private StatusSignal<Boolean> _statorCurrentLimitFault;
+  private StatusSignal<Boolean> _supplyCurrentLimitFault;
 
   public SelfCheckedTalonFX(String name, TalonFX talon) {
     _hardwareFault = talon.getFault_Hardware();
     _forwardSoftLimitFault = talon.getFault_ForwardSoftLimit();
     _reverseSoftLimitFault = talon.getFault_ReverseSoftLimit();
+    _statorCurrentLimitFault = talon.getFault_StatorCurrLimit();
+    _supplyCurrentLimitFault = talon.getFault_SupplyCurrLimit();
 
-    _hardwareFaultAlert = new Alert(name + ": Hardware fault active", AlertType.ERROR);
-    _forwardSoftLimitFaultAlert = new Alert(name + ": Forward soft limit reached", AlertType.WARNING);
-    _reverseSoftLimitFaultAlert = new Alert(name + ": Reverse soft limit reached", AlertType.WARNING); 
+    _hardwareFaultAlert = new Alert(name + ": Hardware fault active.", AlertType.ERROR);
+    _forwardSoftLimitFaultAlert = new Alert(name + ": Forward soft limit reached.", AlertType.INFO);
+    _reverseSoftLimitFaultAlert = new Alert(name + ": Reverse soft limit reached.", AlertType.INFO);
+    _statorCurrentLimitFaultAlert = new Alert(name + ": Stator current limit reached.", AlertType.WARNING);
+    _supplyCurrentLimitFaultAlert = new Alert(name + ": Supply current limit reached.", AlertType.WARNING);
   }
 
   @Override
@@ -44,15 +52,27 @@ public class SelfCheckedTalonFX extends SelfChecked {
       _reverseSoftLimitFaultAlert.set(true);
     }
 
+    if (_statorCurrentLimitFault.getValue()) {
+      _statorCurrentLimitFaultAlert.set(true);
+    }
+
+    if (_supplyCurrentLimitFault.getValue()) {
+      _supplyCurrentLimitFaultAlert.set(true);
+    }
+
     // refresh alerts and faults
     _hardwareFaultAlert.set(false);
     _forwardSoftLimitFaultAlert.set(false);
     _reverseSoftLimitFaultAlert.set(false);
+    _statorCurrentLimitFaultAlert.set(false);
+    _supplyCurrentLimitFaultAlert.set(false);
 
     BaseStatusSignal.refreshAll(
       _hardwareFault,
       _forwardSoftLimitFault,
-      _reverseSoftLimitFault
+      _reverseSoftLimitFault,
+      _statorCurrentLimitFault,
+      _supplyCurrentLimitFault
     );
   }
 }
