bdae2caca20a82727751cd5dcc0bb3844967dee3
PGgit08 <peter.mty8@gmail.com>
2024-09-08 13:50:17 -0400

    found single tag x/y constants for vision heuristic, need to do theta and multi-tag

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -51,7 +51,7 @@ public final class Constants {
         "left-arducam",
         new Transform3d(new Translation3d(0, 0, 1), new Rotation3d()),
         0.2,
-        1
+        0.0001
       )
     );
   }
